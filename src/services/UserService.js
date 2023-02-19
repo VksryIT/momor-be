81612347655680417a0618716284e9c92690ffe0
Jae Young Choi <uniform3000@naver.com>
2023-02-19 17:44:48 +0900

    fix: Add signup feature - 최초 회원가입 시 아이디, 비밀번호로 가입 - db connection pool 설정으로 변경

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,18 +1,34 @@
-import connPool from '../database/connect/maria.js';
+import crypto from 'crypto';
+import util from 'util';
+import connectionPool from '../database/connect/maria.js';
 
+
+const randomBytesPromise = util.promisify(crypto.randomBytes);
+const pbkdf2Promise = util.promisify(crypto.pbkdf2);
+
+const getHashedPasswordAndSalt = async (password) => {
+    const salt = (await randomBytesPromise(64)).toString('base64');
+    const hashedPassword = (await pbkdf2Promise(password, salt, 102542, 64, "sha512")).toString('base64');
+    return { hashedPassword: hashedPassword, salt: salt };
+};
 
 const createUser = async (userInfo) => {
-    let conn, result;
+    let conn;
     try {
-        conn = connPool;
-        result = conn.query(`INSERT INTO user (name) VALUES ('${userInfo.name}')`);
+        const { hashedPassword, salt } = await getHashedPasswordAndSalt(userInfo.password);
+        conn = await connectionPool.getConnection();
+        await conn.beginTransaction();
+        const userInsertResult = await conn.execute(`INSERT INTO user (name) VALUES ('${userInfo.name}')`);
+        await conn.execute(`INSERT INTO user_password (user_no, password, salt) 
+                            VALUES (${userInsertResult[0].insertId}, '${hashedPassword}', '${salt}')`);
+        await conn.commit();
     } catch (error) {
+        if (conn) await conn.rollback();
         throw error;
     } finally {
-        if (conn) conn.end();
+        if (conn) conn.release();
     }
 }
-
 
 export default {
     createUser,
