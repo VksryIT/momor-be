6db7364e8fce5cdfa66bd31038de08859a11dba5
Jae Young Choi <uniform3000@naver.com>
2023-03-12 15:46:05 +0900

    fix: 로그인 시 세션 초기화 추가

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 import * as AuthController from '../controller/AuthController';
+import statusCode from '../modules/statusCode';
 const router = express.Router();
 
-router.post('/login', (req, res, next) => {
+router.post('/login', (req: any, res: any, next) => {
+    if (req.user) {
+        req.logout((err) => {
+            if (err) {
+                return res
+                    .status(statusCode.INTERNAL_SERVER_ERROR)
+                    .send(statusCode.INTERNAL_SERVER_ERROR);
+            }
+        });
+    }
     AuthController.authLogin(req, res, next);
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout', (req: any, res: any) => {
     AuthController.authLogout(req, res);
 });
 
